f318a7aba8cb83b63edbb3201abd6e5031cc5dfb
isabellabider <23ibider@castilleja.org>
2023-05-18 09:41:52 -0700

    trying to initialize a string array and getting the general structure set up? a little lost though

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,35 @@
 public class ScrambleGame {
-    private static final String [] WORDS_DATABASE = new String [] {
-        "fungus", "meow", "juice", "rock", "scrumptious"
+    private static final String [] WORD_BANK = new String [] {
+        "fungus", "meow", "juice", "rock", "scrumptious", "teletubbies"
     };
 
     public static void main(String[] args){
-        ScrambleGame sg = new ScrambleGame(); //is this right?
-        sg.startGame(); //is this right?
+        ScrambleGame sg = new ScrambleGame();
+        sg.startGame();
     }
     private void startGame () {
+        int NumberOfGuesses = 0;
+        while (??) {
+            System.out.println("The shuffled word is: "); //how do i make it choose a random word from the word bank?
+            //some stuff should go here/
+            if (original.equals(??)) {
+                System.out.println("Nice job you unscrammbled the word");
+            } else {
+                System.out.println("You are wrong.");
+            }
+        }
+    }
+    public String Guess () { //have the ueser guess the original word
+            System.out.println("Type in what you think the word is unscrammbled.");
+            //other stuff??
+    }
 
-}
+    public String selectWord() { //select a word from the word bank
+            return WORD_BANK; //??
+    }
 
-    public String getUserGuess() {
-
+    public String ShuffledWord() {
+        String ShuffleWords = ogWord; //og word before ze shuffling
+        //stuff idk
+        return ShuffledWord;
 }
