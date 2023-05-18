ab36ff63325e4358f1f5be764c9e4d7d038e1a36
isabellabider <23ibider@castilleja.org>
2023-05-18 09:01:04 -0700

    trying to initialize a string array? a little lost though

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class ScrambleGame {
+    private static final String [] WORDS_DATABASE = new String [] {
+        "fungus", "meow", "juice", "rock", "scrumptious"
+    };
+
+    public static void main(String[] args){
+        ScrambleGame sg = new ScrambleGame(); //is this right?
+        sg.startGame(); //is this right?
+    }
+    private void startGame () {
+
+}
+
+    public String getUserGuess() {
+
+}
