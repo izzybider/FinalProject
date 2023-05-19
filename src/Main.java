9c14cde69f512a25520e56a8587a0f7085481a9b
isabellabider <23ibider@castilleja.org>
2023-05-19 15:16:24 -0700

    trying to initialize a string array and getting the general structure set up? a little lost though

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,51 @@
-public class ScrambleGame {
-    private static final String [] WORD_BANK = new String [] {
-        "fungus", "meow", "juice", "rock", "scrumptious", "teletubbies"
-    };
+import processing.core.PApplet;
 
-    public static void main(String[] args){
-        ScrambleGame sg = new ScrambleGame();
-        sg.startGame();
+public class ScrambleGame extends PApplet {
+    private final String[] WORD_BANK = {"fungus", "meow", "juice", "rock", "scrumptious"};
+
+    public static void main(String[] args) {
+        PApplet.main("ScrambleGame");
     }
-    private void startGame () {
+
+    public ScrambleGame() {
+    }
+
+    public void settings() {
+        size(400, 400);
+    }
+
+    public void draw() {
+        startGame();
+        noLoop(); //draw is called just once
+    }
+
+    public void mouseClicked() {
+        redraw();
+    }
+
+    public String selectWord() {
+        return WORD_BANK; // how do i have the program select a random word from the bank?
+    }
+
+    public String Guess() { //have the ueser guess the original word
+        System.out.println("Type in what you think the word is unscrammbled.");
+        //other stuff??
+    }
+    private void startGame() {
         int NumberOfGuesses = 0;
-        while (??) {
-            System.out.println("The shuffled word is: "); //how do i make it choose a random word from the word bank?
-            //some stuff should go here/
-            if (original.equals(??)) {
-                System.out.println("Nice job you unscrammbled the word");
-            } else {
-                System.out.println("You are wrong.");
-            }
+        String ogWord = selectWord(); //??
+        System.out.println("The shuffled word is: "); //how do i make it choose a random word from the word bank?
+        Guess++; //idk how to put this, but i want the program to have the user guess?
+        if (ogWord.equals(Guess)) {
+            System.out.println("Nice job you unscrammbled the word");
+        } else {
+            System.out.println("You are wrong.");
+            //need to make it stop letting the user guess after 3 guesses
         }
     }
-    public String Guess () { //have the ueser guess the original word
-            System.out.println("Type in what you think the word is unscrammbled.");
-            //other stuff??
-    }
-
-    public String selectWord() { //select a word from the word bank
-            return WORD_BANK; //??
-    }
-
     public String ShuffledWord() {
         String ShuffleWords = ogWord; //og word before ze shuffling
         //stuff idk
         return ShuffledWord;
+    }
 }
